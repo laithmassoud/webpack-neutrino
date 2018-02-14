89e71f61bb2ac88138a3f6de8efcf441137a33d9
Laith <laythmassoud8@gmail.com>
2018-02-14 13:26:19 +0100

    new project

---
+++ .neutrinorc.js
@@ -0,0 +1,25 @@
+module.exports = {
+ options:{
+      output: 'docs'
+ },
+  use: [
+    '@neutrinojs/airbnb-base',
+    [
+      '@neutrinojs/web',
+      {
+        style: {
+          loaders:[
+          {
+            loader:'sass-loader',
+          }
+          ]
+        }
+      },
+      {
+        html: {
+          title: 'neutrino-reload'
+        }
+      }
+    ]
+  ]
+};
